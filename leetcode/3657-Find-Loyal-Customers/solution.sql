b6822715b0df09fe6c025879abbbb0dcb3906f5d
Yubin <yubin.jeong021@gmail.com>
2025-09-01 14:41:29 +0900

    Update solution.sql

--- leetcode/3657-Find-Loyal-Customers/solution.sql
+++ leetcode/3657-Find-Loyal-Customers/solution.sql
@@ -5,5 +5,3 @@ HAVING COUNT(CASE WHEN transaction_type = 'purchase' THEN 1 END) >= 3
 AND DATEDIFF(MAX(transaction_date), MIN(transaction_date)) >= 30
 AND COUNT(CASE WHEN transaction_type = 'refund' THEN 1 END) / COUNT(*) < 0.2
 ORDER BY customer_id;
-
--- https://leetcode.com/problems/find-loyal-customers/?envType=problem-list-v2&envId=n4wrvgr7
